a7322f6fb78a940c1d3dae9e9fd1ad10044a86ac
DarkMike529 <darkmike529@gmail.com>
2025-07-04 12:01:53 -0600

    Testing JFrame

--- src/screens/VentanaPrincipal.java
+++ src/screens/VentanaPrincipal.java
@@ -1,12 +1,31 @@
 package screens;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class VentanaPrincipal extends JFrame {
+
+    private JMenuBar menuBar;
+    private JMenu menuAlumnos;
+    private JMenuItem menuInsertarAlumnos;
+
+
     public VentanaPrincipal(String title){
         this.setTitle(title);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(800, 700);
+        initComponents();
     }
-    
+
+    private void initComponents(){
+        menuBar = new JMenuBar();
+
+        menuAlumnos = new JMenu();
+        menuAlumnos.setText("Alumnos");
+
+        menuInsertarAlumnos = new JMenuItem();
+        menuInsertarAlumnos.setText("Insertar...");
+    }
 }
